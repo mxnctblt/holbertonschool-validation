6cd5341c24b24fba6234d703913c616f268dd772
mxnctblt <thibaultmaxence44@gmail.com>
2023-05-22 02:19:56 -0700

    fix files

--- module3_task5/setup.sh
+++ module3_task5/setup.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-apt-get update && apt-get install -y make wget
+sudo apt-get update && sudo apt-get install -y make git zip
 
 # download/install minimal version to use template ananke
-wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz
-tar -xvf hugo_extended_0.109.0_Linux-64bit.tar.gz hugo
-mv hugo /usr/local/bin/
-
+sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
+sudo apt install ./hugo.deb
 #remove file after installation
-rm hugo_extended_0.109.0_Linux-64bit.tar.gz
+sudo rm hugo.deb
 
 # download/install linter
-go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.52.2
-npm install -g markdownlint-cli
-npm install -g markdown-link-check
-
+sudo go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.52.2
+sudo npm install -g markdownlint-cli
+sudo npm install -g markdown-link-check
